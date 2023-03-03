214f755abb52468227c4f071317d4bf4039adb56
Hackintosh 5 <git@hack5.dev>
2023-03-03 15:20:07 +0000

    Delete palshop

--- src/components/Error.tsx
+++
@@ -1,8 +0,0 @@
-export default function Error(
-    {text}: {text?: string}
-) {
-    if (!text) return <></>
-    return <p className="help is-danger">
-        {text}
-    </p>
-}
--- src/components/HomeLink.tsx
+++
@@ -1,7 +0,0 @@
-import { Link } from "react-router-dom"
-
-export default function HomeLink() {
-    return <Link to="/" className="button mb-4">
-        Back home
-    </Link>
-}
